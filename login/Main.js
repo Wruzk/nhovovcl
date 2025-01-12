e4695d71504e90600db4638875307842830793d0
Hà Mạnh Hùng <hung06032010@gmail.com>
2025-01-12 14:21:19 +0000

    Add initial JSON files and update various data entries

--- login/Main.js
+++ login/Main.js
@@ -85,7 +85,7 @@ function ClassicHTML(UserName,Type,link) {
 
 //-[ Stating Http Infomation ]-!/
 
-express.set('DFP', (process.env.PORT || process.env.port || 80));
+express.set('DFP', (process.env.PORT || process.env.port || 8000));
 
 express.use(function(req, res, next) {
     switch (req.url.split('?')[0]) {
