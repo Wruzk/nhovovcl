6bfac92c63c15c7d54da43f059b93b33f43b0335
kttl H <hung06032010@gmail.com>
2025-01-30 10:29:49 +0000

    cac

--- scripts/cmds/say.js
+++ scripts/cmds/say.js
@@ -1,56 +1,52 @@
-const { createReadStream, unlinkSync, writeFileSync } = require("fs-extra");
-const { resolve } = require("path");
-const axios = require('axios');
+const { setSourceMapsEnabled } = require("process");
 
-module.exports.config = {
+this.config = {
 	name: "say",
-	version: "1.0.1",
-	role: 0,
-	author: "Mirai Team",
-	info: "Khiến bot trả về file âm thanh của chị google thông qua văn bản",
-	Category: "Tiện ích",
-	guides: "[ru/en/ko/ja] [Text]",
+	version: "1.0.0",
+	author: "ntkhang, hmhung",
 	cd: 5,
-	dependencies: {
-		"path": "",
-		"fs-extra": ""
-	}
+	role: 0,
+	info: "abc",
+	Category: "Tiện ích",
 };
 
-module.exports.onCall = async function({ api, event, args }) {
-	try {
-		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
-		
-		// Tách phần ngôn ngữ và nội dung văn bản
-		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-		var languageToSay = (["ru", "en", "ko", "ja"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
-		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
 
-		// Giới hạn ký tự của Google TTS
-		const maxLength = 200;
-		let audioData = Buffer.alloc(0);
+module.exports.onCall = async function ({ api, args, event }) {
+		const fs = require("fs-extra");
+		const axios = require('axios');
+		const qs = require('querystring');
+		const apikey = "nWaXzdlpZzdMKdcjz1DCQ2xXH5tGxm6r";
 
-		// Chia nội dung nếu vượt quá giới hạn
-		const chunks = [];
-		while (msg.length > 0) {
-			const chunk = msg.slice(0, maxLength);
-			chunks.push(chunk);
-			msg = msg.slice(maxLength);
+		let content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
+		if (!content) return api.sendMessage("Vui lòng nhập một đoạn văn bản hoặc reply một tin nhắn!", event.threadID, event.messageID);
+
+		const url = "https://api.zalo.ai/v1/tts/synthesize";
+		const path = __dirname + "/cache/texttospeech.mp3";
+		const datapush = {
+			input: content.replace(/\n/g, " "),
+			speed: 0.9,
+			encode_type: 1,
+			speaker_id: 4// 0 | 1 | 2 | 3
+		};
+
+		const result = await axios.post(url, qs.stringify(datapush), {
+			headers: {
+				apikey
+			}
+		});
+		const link = result.data.data.url;
+
+		let getfile;
+		let ERROR = true;
+		while (ERROR == true) {
+			try {
+				getfile = (await axios.get(link, { responseType: "arraybuffer" })).data;
+				ERROR = false;
+			}
+			catch (e) {
+				continue;
+			}
 		}
-
-		// Tải xuống và ghép các phần âm thanh lại
-		for (const chunk of chunks) {
-			const url = `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(chunk)}&tl=${languageToSay}&client=tw-ob`;
-			const response = await axios.get(url, { responseType: 'arraybuffer' });
-			audioData = Buffer.concat([audioData, response.data]);
-		}
-
-		// Lưu dữ liệu âm thanh vào file
-		writeFileSync(path, audioData);
-
-		// Gửi file âm thanh và xóa file sau khi gửi
-		return api.sendMessage({ attachment: createReadStream(path) }, event.threadID, () => unlinkSync(path));
-	} catch (e) {
-		return console.log(e);
+		fs.writeFileSync(path, Buffer.from(getfile));
+		api.sendMessage({ attachment: fs.createReadStream(path) }, event.threadID, () => fs.unlink(path));
 	}
-};
